a3833e9b6164423b7a815c1c9acd68cfb2eabc53
Fahim Khan <fk652@nyu.edu>
2023-06-08 16:31:02 -0400

    Time: 410 ms (37.64%), Space: 47.9 MB (43.46%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -3,45 +3,30 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-        let n = s.length;
+    let longest = "";
     
-        let table = new Array(n);
-        for(let i = 0; i < n; i++)
-        {
-            table[i] = new Array(n);
-        }
-  
-        let maxLength = 1;
-        for (let i = 0; i < n; ++i)
-            table[i][i] = true;
-
-        let start = 0;
-        for (let i = 0; i < n - 1; ++i)
-        {
-            if (s[i] == s[i + 1])
-            {
-                table[i][i + 1] = true;
-                start = i;
-                maxLength = 2;
-            }
-        }
-  
-        for (let k = 3; k <= n; ++k) {
-            for (let i = 0; i < n - k + 1; ++i)
-            {
-                let j = i + k - 1;
-  
-                if (table[i + 1][j - 1]
-                    && s[i] == s[j]) {
-                    table[i][j] = true;
-  
-                    if (k > maxLength) {
-                        start = i;
-                        maxLength = k;
-                    }
+    for (let i = 0; i < s.length; i++) {
+        if (s.length - i < longest.length) break;
+        
+        for (let j = s.length - 1; j >= i; j--) {
+            if (j + 1 - i < longest.length) break;
+            
+            if (s[i] === s[j]) {
+                const slice = s.slice(i, j+1);
+                if (isPalindrome(slice) && slice.length > longest.length) {
+                    longest = slice;
+                    break;
                 }
             }
         }
+    }
     
-        return s.slice(start, start + maxLength);
+    return longest;
 };
+
+const isPalindrome = (str) => {
+    for (let i = 0; i < Math.floor(str.length / 2); i++) {
+        if (str[i] !== str[str.length - (i + 1)]) return false;
+    }
+    return true;
+}
