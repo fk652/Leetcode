9205c79177ae21b9291dde658540221839a575b7
Fahim Khan <fk652@nyu.edu>
2023-06-08 15:57:26 -0400

    Time: 4728 ms (9.09%), Space: 48.2 MB (40.37%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    let longest = "";
+    
+    for (let i = 0; i < s.length; i++) {
+        for (let j = s.length - 1; j >= i; j--) {
+            if (s[i] === s[j]) {
+                const slice = s.slice(i, j+1);
+                if (isPalindrome(slice) && slice.length > longest.length) {
+                    longest = slice;
+                    break;
+                }
+            }
+        }
+    }
+    
+    return longest;
+};
+
+const isPalindrome = (str) => {
+    for (let i = 0; i < Math.floor(str.length / 2); i++) {
+        if (str[i] !== str[str.length - (i + 1)]) return false;
+    }
+    return true;
+}
