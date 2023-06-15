aa9908ecf014c03b9486b2597bab1fc8fafc8384
Fahim Khan <fk652@nyu.edu>
2023-06-15 10:47:21 -0400

    Time: 127 ms (55.67%), Space: 51 MB (10.91%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+    const romanNumerals = {
+        'M': 1000,
+        'CM': 900,
+        'D': 500,
+        'CD': 400,
+        'C': 100,
+        'XC': 90,
+        'L': 50,
+        'XL': 40,
+        'X': 10,
+        'IX': 9,
+        'V': 5,
+        'IV': 4,
+        'I': 1
+    };
+    
+    let romanStr = '';
+    
+    for (const [numeral, val] of Object.entries(romanNumerals)) {
+        if (num < val) continue;
+        
+        const times = Math.floor(num / val);
+        romanStr += numeral.repeat(times);
+        
+        num = num % val;
+    }
+    
+    return romanStr;
+};
