3a301309ea9a52beaeb1ae622f739d0905e807b8
Fahim Khan <fk652@nyu.edu>
2023-06-20 16:15:26 -0400

    Time: 69 ms (28.01%), Space: 44 MB (51.25%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    const words = s.split(' ').filter(word => word.length > 0);
+    return words.reverse().join(' ');
+};
