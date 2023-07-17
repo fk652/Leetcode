b1aa62f30cdec413d98702c02b62b6af90edf3b6
Fahim Khan <fk652@nyu.edu>
2023-07-17 16:25:43 -0400

    Time: 96 ms (25.00%), Space: 211.1 MB (25.00%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.rb
+++ 0075-sort-colors/0075-sort-colors.rb
@@ -1,29 +1,20 @@
 # @param {Integer[]} nums
 # @return {Void} Do not return anything, modify nums in-place instead.
-def sort_colors(nums, i = 0, j = nums.length-1)
-    return nums if i >= j
-    
-    mid = (j + i) / 2
-    left = sort_colors(nums, i, mid)
-    right = sort_colors(nums, mid+1, j)
-    
-    return merge(nums, i, mid, mid+1, j)
-end
-
-def merge(nums, left_i, left_j, right_i, right_j)
-    while left_i <= left_j && right_i <= right_j
-        if nums[left_i] > nums[right_i]
-            j = right_i
-            while j != left_i
-                nums[j], nums[j-1] = nums[j-1], nums[j] 
-                j -= 1
-            end
-            
-            left_i += 1
-            right_i += 1
-            left_j += 1
+def sort_colors(nums)
+    low = 0
+    mid = 0
+    high = nums.length - 1
+        
+    while mid <= high
+        if nums[mid] == 0
+            nums[low], nums[mid] = nums[mid], nums[low]
+            low += 1
+            mid += 1
+        elsif nums[mid] == 1
+            mid += 1
         else
-            left_i += 1
+            nums[high], nums[mid] = nums[mid], nums[high]
+            high -= 1
         end
     end
     
