dcda918c754042649e7c21c992573db2c68c54e1
Fahim Khan <fk652@nyu.edu>
2023-08-24 18:26:04 -0400

    Time: 157 ms (55.08%), Space: 90.8 MB (85.23%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.js
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.js
@@ -11,27 +11,27 @@
  * @return {number}
  */
 var longestZigZag = function(root) {
-    let longest = 0;
-    
-    var zigZagHelper = function(root, moveLeft, currentLength) {
-        if (!root) return;
-
-        longest = Math.max(longest, currentLength);
-
-        if (moveLeft) {
-            //go right but also consider left node as new starting point going left
-            zigZagHelper(root.right, !moveLeft, currentLength + 1);
-            zigZagHelper(root.left, moveLeft, 1);
-        } else {
-            //go left but also consider right node as new starting point going right
-            zigZagHelper(root.left, !moveLeft, currentLength + 1);
-            zigZagHelper(root.right, moveLeft, 1);
-        }
-    }
-
     // try left and right from root node
-    zigZagHelper(root, true, 0);
-    zigZagHelper(root, false, 0);
+    const left = zigZagHelper(root, true, -1);
+    const right = zigZagHelper(root, false, -1);
     
-    return longest;
+    return Math.max(left, right);
 };
+
+var zigZagHelper = function(root, moveLeft, length) {
+    if (!root) return length;
+
+    if (moveLeft) {
+        //go right but also consider left node as new starting point going left
+        const zigRight = zigZagHelper(root.right, !moveLeft, length + 1);
+        const newLeft = zigZagHelper(root.left, moveLeft, 0);
+        length = Math.max(length, zigRight, newLeft)
+    } else {
+        //go left but also consider right node as new starting point going right
+        const zigLeft = zigZagHelper(root.left, !moveLeft, length + 1);
+        const newRight = zigZagHelper(root.right, moveLeft, 0);
+        length = Math.max(length, zigLeft, newRight);
+    }
+    
+    return length;
+}
