2c9b0513548f20c1d05a2c679fedced57fc3ad35
Fahim Khan <fk652@nyu.edu>
2023-08-28 13:07:32 -0400

    Time: 251 ms (94.22%), Space: 117.7 MB (77.46%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.js
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.js
@@ -12,14 +12,18 @@ var minReorder = function(n, connections) {
     while (queue.length) {
         const node = queue.shift();
 
-        for (const neighbor of invertedGraph[node]) {
+        // go over normal graph edges first to get edges leading to node 0
+        // e.g all neighbors in graph[0] are nodes that lead directly to 0
+        // so all other edges branching out from there also lead to node 0
+        for (const neighbor of graph[node]) {
             if (!visited.has(neighbor)) {
                 visited.add(neighbor);
                 queue.push(neighbor);
             }
         }
 
-        for (const neighbor of graph[node]) {
+        // any inverted edges leading to unvisited nodes are the ones to be reordered
+        for (const neighbor of invertedGraph[node]) {
             if (!visited.has(neighbor)) {
                 visited.add(neighbor);
                 queue.push(neighbor);
@@ -40,9 +44,10 @@ var buildGraphs = function(n, connections) {
         inverted[i] = [];
     }
     
+    // connection means A leads to B, meaning A is a neighbor of B
     for (const [a, b] of connections) {
-        graph[a].push(b);
-        inverted[b].push(a);
+        graph[b].push(a);
+        inverted[a].push(b);
     }
     
     return [graph, inverted];
