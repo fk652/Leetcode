172447a985076e19ab8776a6e67f5819c4ab3d34
Fahim Khan <fk652@nyu.edu>
2023-08-31 12:02:35 -0400

    Time: 60 ms (39.55%), Space: 42.8 MB (79.10%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.js
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.js
@@ -3,6 +3,28 @@
  * @return {number}
  */
 
+// simpler version to previous solution below using bottom up approach
+// the pattern is f(n) = (2 * f(n-1)) + f(n-3)
+// final answer needs to be mod (10**9) + 7
+var numTilings = function(n, state = 0, memo = {}) {
+    const MOD = (10**9) + 7;
+    const dp = {};
+    
+    // base cases
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 5
+    
+    for (let i = 4; i <= n; i++) {
+        dp[i] = (2 * dp[i-1]) + dp[i-3];
+        dp[i] %= MOD;
+    }
+    
+    return dp[n];
+};
+
+
+// OLD SOLUTION (top down memoization approach)
  // the board is 2xN, so each column has 2 spots
  // n will represent the current column number
  // we recursively solve for previous columns and add up the ways to fill them
@@ -40,21 +62,21 @@
 // - represents spots to fill/solve for
 
 // state starts at 0 because the final problem needs the entire board filled up
-var numTilings = function(n, state = 0, memo = {}) {
-    if (n < 3) return n;
+// var numTilings = function(n, state = 0, memo = {}) {
+//     if (n < 3) return n;
     
-    const key = `${n} ${state}`;
-    if (key in memo) return memo[key];
+//     const key = `${n} ${state}`;
+//     if (key in memo) return memo[key];
 
-    if (state === 0) {
-        memo[key] = numTilings(n-1, 0, memo) + numTilings(n-2, 0, memo) + numTilings(n-2, 1, memo) + numTilings(n-2, 2, memo);
-    } else if (state === 1) {
-        memo[key] = numTilings(n-1, 0, memo) + numTilings(n-1, 2, memo);
-    } else if (state === 2) {
-        memo[key] = numTilings(n-1, 0, memo) + numTilings(n-1, 1, memo);
-    }
+//     if (state === 0) {
+//         memo[key] = numTilings(n-1, 0, memo) + numTilings(n-2, 0, memo) + numTilings(n-2, 1, memo) + numTilings(n-2, 2, memo);
+//     } else if (state === 1) {
+//         memo[key] = numTilings(n-1, 0, memo) + numTilings(n-1, 2, memo);
+//     } else if (state === 2) {
+//         memo[key] = numTilings(n-1, 0, memo) + numTilings(n-1, 1, memo);
+//     }
     
-    // final answer must be returned modulo ((10**9) + 7)
-    memo[key] = memo[key] % ((10**9)+7);
-    return memo[key];
-};
+//     // final answer must be returned modulo ((10**9) + 7)
+//     memo[key] = memo[key] % ((10**9)+7);
+//     return memo[key];
+// };
