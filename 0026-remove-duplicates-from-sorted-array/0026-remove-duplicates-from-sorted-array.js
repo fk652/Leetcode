46b7dd90d58139799c5d8aeb9a3137336ab67f27
Fahim Khan <fk652@nyu.edu>
2023-09-01 16:11:23 -0400

    Time: 65 ms (67.22%), Space: 44.5 MB (68.51%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,21 +3,35 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    // starting from 2nd number
-    let count = 1;
-    let i = 1;
+    //involves swapping numbers making the first k numbers all unique
+    let i = 0;
     
-    // remove if same as previous number
-    // otherwise move on and increment unique counter and i
-    // don't need to increment i on removal since the next number moves down to nums[i]
-    while (i < nums.length) {
-        if (nums[i] === nums[i-1]) {
-            nums.splice(i, 1);
-        } else {
-            count++;
+    for (let j = 0; j <= nums.length; j++) {
+        if (nums[i] !== nums[j]) {
             i++;
+            nums[i] = nums[j];
         }
     }
     
-    return count;
+    return i;
 };
+
+// var removeDuplicates = function(nums) {
+//     // starting from 2nd number
+//     let count = 1;
+//     let i = 1;
+    
+//     // remove if same as previous number
+//     // otherwise move on and increment unique counter and i
+//     // don't need to increment i on removal since the next number moves down to nums[i]
+//     while (i < nums.length) {
+//         if (nums[i] === nums[i-1]) {
+//             nums.splice(i, 1);
+//         } else {
+//             count++;
+//             i++;
+//         }
+//     }
+    
+//     return count;
+// };
