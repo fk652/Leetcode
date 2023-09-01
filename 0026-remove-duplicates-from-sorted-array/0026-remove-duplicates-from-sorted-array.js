7da2fff305c4671f9d16b68a4fb7fd1c5b033c2c
Fahim Khan <fk652@nyu.edu>
2023-09-01 15:58:07 -0400

    Time: 104 ms (24.83%), Space: 44.6 MB (59.92%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    // starting from 2nd number
+    let count = 1;
+    let i = 1;
+    
+    // remove if same as previous number
+    // otherwise move on and increment unique counter and i
+    // don't need to increment i on removal since the next number moves down to nums[i]
+    while (i < nums.length) {
+        if (nums[i] === nums[i-1]) {
+            nums.splice(i, 1);
+        } else {
+            count++;
+            i++;
+        }
+    }
+    
+    return count;
+};
