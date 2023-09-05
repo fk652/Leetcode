ea9cd491f4aaafbc143ded54883b1bfb591ebee1
Fahim Khan <fk652@nyu.edu>
2023-09-05 13:20:53 -0400

    Time: 1653 ms (5.06%), Space: 221.6 MB (5.06%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -6,14 +6,14 @@
 
 // using top down memoization approach
 // i and j keep track of current index in text1 and text2, respectively
-var longestCommonSubsequence = function(text1, text2, i=0, j=0, memo={}) {
-    // base case is when i or j is >= length of the string, nothing in common so return 0
-    if (i >= text1.length || j >= text2.length) return 0;
-    
+var longestCommonSubsequence = function(text1, text2, i=0, j=0, memo={}) {    
     // use i and j as the memoization key since those always change each recursion
     // also same as memoizing the same substring pairs being compared
     const key = `${i} ${j}`;
     if (key in memo) return memo[key];
+    
+    // base case is when i or j is >= length of the string, nothing in common so return 0
+    if (i >= text1.length || j >= text2.length) return 0;
     
     // if first characters match, find LCS of string without those 2 char and add 1
     // else find the LCS of text1 excluding i with same text2
