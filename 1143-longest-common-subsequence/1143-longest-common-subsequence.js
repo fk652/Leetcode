61c916170e0df407a6f27bdaa6bc60f07b6c64f4
Fahim Khan <fk652@nyu.edu>
2023-09-05 14:31:35 -0400

    Time: 82 ms (96.14%), Space: 48.1 MB (94.85%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -4,28 +4,25 @@
  * @return {number}
  */
 
-// using top down memoization approach
-// i and j keep track of current index in text1 and text2, respectively
-var longestCommonSubsequence = function(text1, text2, i=0, j=0, memo={}) {    
-    // use i and j as the memoization key since those always change each recursion
-    // also same as memoizing the same substring pairs being compared
-    const key = `${i} ${j}`;
-    if (key in memo) return memo[key];
-    
-    // base case is when i or j is >= length of the string, nothing in common so return 0
-    if (i >= text1.length || j >= text2.length) return 0;
-    
-    // if first characters match, find LCS of string without those 2 char and add 1
-    // else find the LCS of text1 excluding i with same text2
-    // and LCS of text2 exlcuding j with same text1
-    // then choose the maximum between those 2 cases
-    if (text1[i] === text2[j]) {
-        memo[key] = 1 + longestCommonSubsequence(text1, text2, i+1, j+1, memo);
-    } else {
-        const excludeI = longestCommonSubsequence(text1, text2, i+1, j, memo);
-        const excludeJ = longestCommonSubsequence(text1, text2, i, j+1, memo);
-        memo[key] = Math.max(excludeI, excludeJ);
+// dp approach keeping track of current and previous rows, to save space
+// O(n*m) approach going through each combo of substring pairs from end to start
+// using i and j to keep track of current index of text1 and text2, respectively
+var longestCommonSubsequence = function(text1, text2) {
+    let prev = Array(text2.length + 1).fill(0);
+    let curr = Array(text2.length + 1).fill(0);
+
+    for (let i = text1.length - 1; i >= 0; i--) {
+        for (let j = text2.length - 1; j >= 0; j--) {
+            if (text1[i] === text2[j]) {
+                curr[j] = 1 + prev[j + 1];
+            } else {
+                curr[j] = Math.max(curr[j + 1], prev[j]);
+            }
+        }
+
+        prev = [...curr];
     }
-    
-    return memo[key];
+
+
+    return curr[0];
 };
