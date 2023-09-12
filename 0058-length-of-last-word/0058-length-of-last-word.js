546570466768323db0b264929ae4b78e15671ae5
Fahim Khan <fk652@nyu.edu>
2023-09-12 14:58:36 -0400

    Time: 58 ms (25.79%), Space: 43.7 MB (5.09%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,21 +3,19 @@
  * @return {number}
  */
 
-// simply keep track of current word length and last found word length
+// keep track of current word length and last found word length
 var lengthOfLastWord = function(s) {
     let currentLength = 0;
     let last = null;
     
     for (const char of s) {
         if (char === ' ') {
-            if (currentLength > 0) last = currentLength;
             currentLength = 0;
         } else {
             currentLength++;
+            last = currentLength;
         }
     }
     
-    // if no space at the end, return current word length
-    // else return last found word length
-    return currentLength > 0 ? currentLength : last;
+    return last;
 };
