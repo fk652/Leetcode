427d3ea4e3738479112636d4d4450a84945d6014
Fahim Khan <fk652@nyu.edu>
2023-09-13 18:23:59 -0400

    Time: 51 ms (65.17%), Space: 41.8 MB (68.55%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,22 +4,22 @@
  * @return {number}
  */
 // using the built in JavaScript method that does the same thing
-var strStr = function(haystack, needle) {
-    return haystack.indexOf(needle);
-};
+// var strStr = function(haystack, needle) {
+//     return haystack.indexOf(needle);
+// };
 
 // find a char in haystack that is same as the first char in needle
 // lookahead by needle.length chars and see if the string slice is the same as needle
 // if true, return the index of that char in haystack
 // return -1 if nothing was found
-// var strStr = function(haystack, needle) {
-//     const n = needle.length;
+var strStr = function(haystack, needle) {
+    const n = needle.length;
     
-//     for (let i = 0; i < haystack.length; i++) {
-//         if (haystack[i] === needle[0] && haystack.slice(i, i + n) === needle) {
-//             return i;
-//         }
-//     }
+    for (let i = 0; i < haystack.length; i++) {
+        if (haystack[i] === needle[0] && haystack.slice(i, i + n) === needle) {
+            return i;
+        }
+    }
     
-//     return -1;
-// };
+    return -1;
+};
