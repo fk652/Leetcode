d683080d7f23e551a2416b57efd7a7b5e9ffe26d
Fahim Khan <fk652@nyu.edu>
2023-09-19 12:24:59 -0400

    Time: 45 ms (89.47%), Space: 42.3 MB (41.75%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -14,38 +14,38 @@
 
 // recursive solution, checking if node values at similar positions match
 // and recursively checking if the left subtrees and right subtrees match
-// var isSameTree = function(p, q) {
-//     if (!p || !q) return !p && !q;
+var isSameTree = function(p, q) {
+    if (!p || !q) return !p && !q;
     
-//     return p.val === q.val &&
-//            isSameTree(p.left, q.left) &&
-//            isSameTree(p.right, q.right);
-// };
+    return p.val === q.val &&
+           isSameTree(p.left, q.left) &&
+           isSameTree(p.right, q.right);
+};
 
 // bfs solution, running bfs on both tree
 // check if the node values at the front of both bfs queues match,
 // since they are similar positions in both trees
-var isSameTree = function(p, q) {
-    const pQueue = [p];
-    const qQueue = [q];
+// var isSameTree = function(p, q) {
+//     const pQueue = [p];
+//     const qQueue = [q];
 
-    while (pQueue.length || qQueue.length) {
-        if (pQueue[0]?.val !== qQueue[0]?.val) {
-            return false;
-        }
+//     while (pQueue.length || qQueue.length) {
+//         if (pQueue[0]?.val !== qQueue[0]?.val) {
+//             return false;
+//         }
 
-        const pNode = pQueue.shift();
-        if (pNode) {
-            pQueue.push(pNode.left);
-            pQueue.push(pNode.right);
-        }
+//         const pNode = pQueue.shift();
+//         if (pNode) {
+//             pQueue.push(pNode.left);
+//             pQueue.push(pNode.right);
+//         }
 
-        const qNode = qQueue.shift();
-        if (qNode) {
-            qQueue.push(qNode.left);
-            qQueue.push(qNode.right);
-        }
-    }
+//         const qNode = qQueue.shift();
+//         if (qNode) {
+//             qQueue.push(qNode.left);
+//             qQueue.push(qNode.right);
+//         }
+//     }
 
-    return true;
-};
+//     return true;
+// };
