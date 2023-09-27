dba7508965a255b7115e355ef9592e95f09fa8b3
Fahim Khan <fk652@nyu.edu>
2023-09-27 14:45:21 -0400

    Time: 55 ms (71.91%), Space: 43.9 MB (65.53%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+// Modify jump game I solution to count min jumps,
+// by keeping track of the next best spot to jump from.
+
+// Increment jump count whenever the next jump spot is passed,
+// and also update next jump spot to the current highest reachable spot.
+
+// Always update the current highest reachable spot, 
+// whenever current spot + jump distance is better.
+
+var jump = function(nums) {
+    let nextJumpSpot = 0;
+    let highestReachable = 0;
+    let jumps = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        // This problem guarantees the end is reachable, so no need for this check.
+        // if (i > highestReachable) return -1;
+
+        if (i > nextJumpSpot) {
+            nextJumpSpot = highestReachable;
+            jumps++;
+        }
+
+        highestReachable = Math.max(highestReachable, i + nums[i]);
+    }
+
+    return jumps;
+};
