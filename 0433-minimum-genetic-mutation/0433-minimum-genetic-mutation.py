4e4a8201161833b6d5f7594dea1f012495e7a53b
Fahim Khan <fk652@nyu.edu>
2023-10-05 14:25:24 -0400

    Time: 42 ms (27.56%), Space: 16.3 MB (36.97%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -0,0 +1,51 @@
+# bfs graph solution to find the shortest path from startGene to endGene
+# edges are valid one letter mutations between gene strings
+# first build a graph of valid mutations for each bank gene and startGene
+# then run normal bfs to find the shortest path to endGene
+# if no path exists, return -1
+# use a visited set to prevent infinite cycles
+
+class Solution:
+    def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
+        graph = self.buildGraph([*bank, startGene])
+        queue = [startGene]
+        visited = set()
+        count = 0
+        
+        while len(queue) > 0:
+            for _ in range(len(queue)):
+                gene = queue.pop(0)
+                if gene == endGene:
+                    return count
+                
+                for neighbor in graph[gene]:
+                    if neighbor not in visited:
+                        queue.append(neighbor)
+                        visited.add(neighbor)
+            count += 1
+            
+        return -1
+            
+    # helper function to build the graph
+    def buildGraph(self, edges):
+        graph = defaultdict(list)
+        
+        for i in range(len(edges)):
+            for j in range(i+1, len(edges)):
+                if self.isMutation(edges[i], edges[j]):
+                    graph[edges[i]].append(edges[j])
+                    graph[edges[j]].append(edges[i])
+                    
+        return graph
+        
+    # helper function to check if one letter difference between genes
+    def isMutation(self, str1, str2):
+        mutated = False
+        
+        for i in range(len(str1)):
+            if str1[i] != str2[i]:
+                if mutated: 
+                    return False
+                mutated = True
+        
+        return mutated
