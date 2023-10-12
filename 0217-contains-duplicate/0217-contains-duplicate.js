6a0b56345c64fba8ce52a1b6085e582ccf208cfa
Fahim Khan <fk652@nyu.edu>
2023-10-12 13:14:34 -0400

    Time: 90 ms (37.69%), Space: 55.3 MB (26.73%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -2,13 +2,23 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+
+// use a set to keep track of seen numbers
+// if duplicate contained in set, return true
+// return false if no duplicates found
 var containsDuplicate = function(nums) {
-    const numSet = new Set();
-    
+    const seen = new Set();
+
     for (const num of nums) {
-        if (numSet.has(num)) return true;
-        numSet.add(num);
+        if (seen.has(num)) return true;
+        seen.add(num);
     }
-    
+
     return false;
 };
+
+// can simplify code by making a nums set and comparing sizes
+// var containsDuplicate = function(nums) {
+//     const numSet = new Set(nums);
+//     return numSet.size !== nums.length;
+// };
