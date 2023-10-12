18e239f36d146266d03fd29f71b3771c503d3a5b
Fahim Khan <fk652@nyu.edu>
2023-10-12 16:07:28 -0400

    Time: 103 ms (24.80%), Space: 59.1 MB (5.27%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -2,29 +2,48 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+
+// calc the running cumulative products in forward and reverse
+// result[i] = cumulativeProduct[i-1] + reverseCumulativeProduct[i+1]
+// it's the same as getting the products of all numbers before and after nums[i]
+// and multiplying those two products together to get product except self
+// var productExceptSelf = function(nums) {
+//     const n = nums.length;
+//     const cumProd = [nums[0]];
+//     const revCumProd = [nums[n-1]];
+//     for (let i = 1; i < n; i++) {
+//         cumProd.push(nums[i] * cumProd[i-1]);
+//         revCumProd.push(nums[n-1-i] * revCumProd[i-1]);
+//     }
+//     revCumProd.reverse();
+    
+//     const results = [];
+//     for (let i = 0; i < n; i++) {
+//         if (i === 0) results.push(revCumProd[i+1]);
+//         else if (i === n - 1) results.push(cumProd[i-1]);
+//         else results.push(cumProd[i-1] * revCumProd[i+1]);
+//     }
+//     return results;
+// };
+
+// version without reversing revCumProd
+// note that doing revCumProd.unshift instead is worse than .push
+// since unshift is an O(n) operation in JavaScript while push is O(1)
+// so we have to index at revCumProd[n-2-i] instead of revCumProd[i+1]
 var productExceptSelf = function(nums) {
-    const n = nums.length - 1;
-    
-    let totalProductl = 1;
-    const productsl = [];
-    
-    let totalProductr = 1;
-    const productsr = [];
-    
-    for (let i = 0; i <= n; i++) {
-        totalProductl *= nums[i];
-        productsl.push(totalProductl);
-        
-        totalProductr *= nums[n - i];
-        productsr.push(totalProductr);
+    const n = nums.length;
+    const cumProd = [nums[0]];
+    const revCumProd = [nums[n-1]];
+    for (let i = 1; i < n; i++) {
+        cumProd.push(nums[i] * cumProd[i-1]);
+        revCumProd.push(nums[n-1-i] * revCumProd[i-1]);
     }
     
-    const result = [];
-    for (let i = 0; i <= n; i++) {
-        if (i === 0) result.push(productsr[n - 1]);
-        else if (i === n) result.push(productsl[n - 1]);
-        else result.push(productsl[i - 1] * productsr[n - (i + 1)])
+    const results = [];
+    for (let i = 0; i < n; i++) {
+        if (i === 0) results.push(revCumProd[n-2-i]);
+        else if (i === n - 1) results.push(cumProd[i-1]);
+        else results.push(cumProd[i-1] * revCumProd[n-2-i]);
     }
-    
-    return result;
+    return results;
 };
