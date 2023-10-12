6204ea56462e73e37467d1ab97c535c62ba87d66
Fahim Khan <fk652@nyu.edu>
2023-10-12 13:23:12 -0400

    Time: 45 ms (99.79%), Space: 42.6 MB (86.32%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,17 +3,19 @@
  * @param {string} t
  * @return {boolean}
  */
+
+// first check compare s and t lengths (invalid anagram if different lengths)
+// use a hash to keep track of letter counts
+// add 1 for each s char, subtract 1 for each t char
+// return if all final counts are 0 (means s and t have same amount of chars)
 var isAnagram = function(s, t) {
     if (s.length !== t.length) return false;
     
-    const letterCount = {};
+    const count = {};
     for (let i = 0; i < s.length; i++) {
-        s[i] in letterCount ? letterCount[s[i]]++ : letterCount[s[i]] = 1;
-        t[i] in letterCount ? letterCount[t[i]]-- : letterCount[t[i]] = -1;
+        s[i] in count ? count[s[i]]++ : count[s[i]] = 1;
+        t[i] in count ? count[t[i]]-- : count[t[i]] = -1;
     }
     
-    for (let count of Object.values(letterCount)) {
-        if (count !== 0) return false;
-    }
-    return true;
+    return Object.values(count).every(val => val === 0);
 };
