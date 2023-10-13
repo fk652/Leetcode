c1f588c520a6d86b2bd7defc6cd7dcd2a11edbd0
Fahim Khan <fk652@nyu.edu>
2023-10-13 16:42:48 -0400

    Time: 38 ms (99.57%), Space: 42.1 MB (79.70%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -2,22 +2,31 @@
  * @param {string} s
  * @return {boolean}
  */
+
+// use a stack to keep track of opening parantheses characters
+// iterate through all the parantheses characters in the string
+// pop off the top of the stack when a closing parantheses is encountered
+// if not a valid parantheses pair, return false
+// at the end return if stack is empty or not 
+// valid when all parantheses were matched, leaving the stack empty
+
 var isValid = function(s) {
-    const parenStack = [];
-    
-    const closingHash = {
-        ')': '(',
-        '}': '{',
-        ']': '['
+    const paraMap = {
+        ")": "(",
+        "}": "{",
+        "]": "["
     };
+    const paraStack = [];
     
-    for (let char of s) {
-        if (char in closingHash && closingHash[char] === parenStack[parenStack.length-1]) {
-            parenStack.pop();
+    for (const char of s) {
+        if (char in paraMap) {
+            if (paraStack.pop() !== paraMap[char]) {
+                return false;
+            }
         } else {
-            parenStack.push(char);   
+            paraStack.push(char);
         }
     }
     
-    return parenStack.length === 0;
+    return paraStack.length === 0;
 };
