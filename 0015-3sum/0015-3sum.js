014173eef8206702025feea55d53eb049425458e
Fahim Khan <fk652@nyu.edu>
2023-10-17 14:25:49 -0400

    Time: 157 ms (49.58%), Space: 58.8 MB (70.58%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -2,22 +2,40 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+
+// can use 3 pointers for i, j, k
+// for each i find a j,k pair where nums[i] + nums[j] + nums[k] === 0, and i < j < k
+// should also sort the nums arr from least to greatest
+// this helps find a j,k pair faster when comparing nums[i] + nums[j] + nums[k] <=> 0
+// if nums[i] + nums[j] + nums[k] < 0, then increase j to get a bigger number
+// if nums[i] + nums[j] + nums[k] > 0, then decrease k to get a smaller number
+// if nums[i] + nums[j] + nums[k] === 0, then decrease k and increase j
+
+// to avoid duplicate triplets, increment i when same as i-1
+// also whenever nums[i] + nums[j] + nums[k] === 0,
+// keep incrementing/decrementing j/k while same as previous
+
 var threeSum = function(nums) {
     nums.sort((a, b) => a - b);
     const result = [];
     
     for (let i = 0; i < nums.length; i++) {
-        if (i > 0 && nums[i] === nums[i-1]) continue;
+        if (nums[i] === nums[i-1]) continue;
         
         let j = i + 1;
         let k = nums.length - 1;
-        
+
         while (j < k) {
-            if (nums[i] + nums[j] + nums[k] === 0) {
+            const sum = nums[i] + nums[j] + nums[k];
+            if (sum === 0) {
                 result.push([nums[i], nums[j], nums[k]]);
+                
                 j++;
-                while(j < k && nums[j] === nums[j-1]) j++;
-            } else if (nums[i] + nums[j] + nums[k] < 0) {
+                while (j < k && nums[j] === nums[j-1]) j++;
+                
+                k--;
+                while (j < k && nums[k] === nums[k+1]) k--;
+            } else if (sum < 0) {
                 j++;
             } else {
                 k--;
