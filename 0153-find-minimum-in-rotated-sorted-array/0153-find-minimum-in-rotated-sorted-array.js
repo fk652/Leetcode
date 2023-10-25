7c7a438d6f597c851b9d6af1ce97a38ad36fdb16
Fahim Khan <fk652@nyu.edu>
2023-10-24 20:08:39 -0400

    Time: 57 ms (24.05%), Space: 42.1 MB (41.78%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -8,7 +8,12 @@
 // go until low and high reach the same point (the min value)
 // when mid > high (right half is rotated), then search the right half
 // otherwise left half contains the rotation, so search the left half
-// but keep mid when searching the left half
+
+// but keep mid when searching the left half in case that mid is the lowest point
+// because we use Math.floor to round down mid, mid could end up being the min
+// so we don't want to accidently discard it when going down into the left half
+// or go to a point where there are no numbers left (e.g think about the [1, 2] case)
+
 // return nums[high] or nums[low] since that'll be the min point
 
 var findMin = function(nums) {
