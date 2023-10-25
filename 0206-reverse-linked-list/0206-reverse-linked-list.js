eb7e8ce029edbb34cae1b4a4b96f159efa8fa3d2
Fahim Khan <fk652@nyu.edu>
2023-10-25 18:34:13 -0400

    Time: 58 ms (39.07%), Space: 44.3 MB (50.56%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.js
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -9,15 +9,23 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+
+// keep track of a current and previous node
+// go through each node and set a temporary pointer to current.next node
+// set current.next to the previous node
+// then set previous to current and current to the temporary node
+// prev will end up at the new head since current becomes what is after the tail (null)
+// so return prev in the end
+
 var reverseList = function(head) {
     let prev = null;
     let current = head;
     
     while (current) {
-        const next = current.next;
+        const temp = current.next;
         current.next = prev;
         prev = current;
-        current = next;
+        current = temp;
     }
     
     return prev;
