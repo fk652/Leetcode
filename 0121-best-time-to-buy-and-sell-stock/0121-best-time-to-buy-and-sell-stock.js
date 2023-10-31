fa56dd9195bb433d2c1c8bcbf95c8d46cc875d16
Fahim Khan <fk652@nyu.edu>
2023-10-31 15:34:27 -0400

    Time: 85 ms (9.92%), Space: 51.3 MB (80.06%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -2,14 +2,23 @@
  * @param {number[]} prices
  * @return {number}
  */
+
+// go through each price and keep track of current min price (or 0) and max profit
+// update max profit if current price - current min price is greater than current max profit
+// update min price if current price is smaller
+
+// since we go through prices in order from first to last day,
+// and min price comes before current price, and min price being updated after max profit
+// we can safely update max profit by checking current price - min price
+
 var maxProfit = function(prices) {
-    let min = prices[0];
-    let max = 0;
+    let minPrice = prices[0];
+    let maxProfit = 0;
     
-    for (price of prices) {
-        max = Math.max(max, price - min);
-        min = Math.min(min, price);
+    for (const price of prices) {
+        maxProfit = Math.max(maxProfit, price - minPrice);
+        minPrice = Math.min(minPrice, price);
     }
     
-    return max;
+    return maxProfit;
 };
